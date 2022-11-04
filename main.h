9f285edf40185e67867ac2e3aed4dc705b694660
ellakaburu <kaburuelena@gmail.com>
2022-11-04 06:43:09 -0700

    main function

--- main.h
+++ main.h
@@ -1,3 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Gedeon Obae Gekonge
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x08-recursion directory.
+ */
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -8,3 +18,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+
+#endif
