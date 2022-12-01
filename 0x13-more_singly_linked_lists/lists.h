0227a78e9b1bb3ce8190b0f51c86a98709cbba29
ellakaburu <kaburuelena@gmail.com>
2022-12-01 05:27:41 -0800

    more singly linked lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,8 @@
-#ifndef MORE_SINGLY_LINKED_LIST_H_
-#define MORE_SINGLY_LINKED_LIST_H_
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 /**
  * struct listint_s - singly linked list
@@ -13,9 +14,10 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
+
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -28,6 +30,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-listint_t *reverse_listint(listint_t **head);
 
-#endif /* #ifndef MORE_SINGLY_LINKED_LIST_H_ */
+#endif
