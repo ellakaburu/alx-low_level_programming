7646321256f1e883aeb376081e6036ed0c117505
ellakaburu <kaburuelena@gmail.com>
2022-12-02 15:16:56 -0800

    correct

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -9,14 +9,14 @@
  * @n: integer
  * @next: points to the next node
  *
- * Description: singly linked list node structure
- * 
+ * Description: singly linked list node structure 
  */
 typedef struct listint_s
-{
-int n;
-struct listint_s *next;
-} listint_t;
+	{
+		int n;
+		struct listint_s *next;
+	} 
+listint_t;
 
 
 size_t print_listint(const listint_t *h);
